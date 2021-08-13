0b065adae7bb0aae9ae001b6d2c257e44885e39f
martinlopez <martin.m.lopez@azcarnivores.com>
2021-08-13 11:35:37 -0700

    getter and setter for dataset and verbose on compose.py

--- models/compose.py
+++ models/compose.py
@@ -50,11 +50,34 @@ class ComposeV1():
                                         #    2  : Plots when possible and Command line progress updates
         _data = []                      # [CELL] cell array of timesteps each containing a matrix N instances x D features
         _lables = []                    # [CELL] cell array of timesteps each containing a vector N instances x 1 - Correct label
-        _hypothesis =[]                 # [CELL] cell array of timesteps each containing a N instances x 1 - binary vector indicating if instance is a core support (1) or not (0)
+        _hypothesis =[]                 # [CELL] cell array of timesteps each containing a N instances x 1 - Classifier hypothesis
+        _core_support = []              # [CELL] cell array of timesteps each containing a N instances x 1 - binary vector indicating if instance is a core support (1) or not (0)
+        _classifier_func = []           # [STRING] string corresponding to classifier in ssl class
+        _classifier_opts = []           # [STRUCT] struct of options for the selected classifer in ssl class
+
+        _cse_func = []                  # [STRING] string corresponding to function in cse class
+        _cse_opts = []                  # [STRUCT] struct of options for the selected cse function in cse class
+
+        _performance = []               # [VECTOR] column vector of classifier performances at each timestep
+        _comp_time = []                 # [MATRIX] matrix of computation time for column 1 : ssl classification, column 2 : cse extraction
+
+        _dataset = []
+
         self.classifier = classifier
 
-    def validateComposeAttributes(self, dataset, verbose, *args):
+    def getCompose(self, dataset, verbose):
         """
+        Gets dataset and verbose (the command to display options as COMPOSE processes)
+        Verbose: 0 : no info is displayed
+                 1 : Command Line progress updates
+                 2 : Plots when possible and Command Line progress updates
+        """
+        self._dataset = dataset
+        self._verbose = verbose
+
+    def setCompose(self, dataset, verbose, *args):
+        """
+        Sets COMPOSE dataset and information processing options
         Check if the input parameters are not empty for compose
         This checks if the dataset is empty and checks what option of feedback you want
         """
@@ -68,13 +91,16 @@ class ComposeV1():
         # set object displayed info setting
         if self._verbose >= 0 and self._verbose <=2:
            self._verbose = verbose 
-           return verbose
         else:
             print("Only 3 options to display information: 0 - No Info ; 1 - Command Line Progress Updates; 2 - Plots when possilbe and Command Line Progress")
-        
 
+        if not self._dataset:
+            print("Dataset is empty!")
+        else:
+            self._dataset = dataset
 
-
+        return dataset, verbose
+    
 
     def run(self, Xt, Yt, Ut): 
         """
