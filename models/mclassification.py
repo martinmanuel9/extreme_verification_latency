42bdc3354c81c6e40ee39fd096811e0363789684
martinmanuel9 <martinmlopez@arizona.edu>
2022-11-18 17:51:45 -0700

    mclassification - classifer cmplte, centroid inwrk

--- models/mclassification.py
+++ models/mclassification.py
@@ -37,6 +37,8 @@ import numpy as np
 import benchmark_datagen as bdg
 from sklearn.cluster import Birch, KMeans
 from sklearn.mixture import GaussianMixture as GMM
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.svm import SVC, SVR
 from scipy import stats
 from matplotlib import pyplot as plt
 
@@ -44,15 +46,18 @@ class MClassification():
     def __init__(self, 
                 classifier,
                 dataset,
+                method,
                 NClusters:int = 10): 
         """
         """
         self.classifier = classifier
         self.dataset = dataset
         self.NClusters = NClusters
+        self.method = method
         self.class_cluster = {}
         self.mcluster = {}
         self._initialize()
+        
     
     def _setData(self):
         set_data = bdg.Datagen()
@@ -108,31 +113,37 @@ class MClassification():
         """
         self._setData()
         # initial set of cluster based on inital labeled data T; using next time step from datastream for preds
-        if self.classifier == 'kmeans':
+        if self.method == 'kmeans':
             kmeans_model = KMeans(n_clusters=self.NClusters)
             kmeans_model.fit(self.T)
-            preds = kmeans_model.predict(self.Y[1])
-        elif self.classifier == 'gmm':
+            self._euclidean_distance(inCluster = kmeans_model, fitCluster = kmeans_model.fit(self.T), x= self.X[0] , y=self.T )
+            self.preds = kmeans_model.predict(self.Y[1])
+            self.cluster = kmeans_model.cluster_centers_
+        elif self.method == 'gmm':
             gmm_model = GMM(n_components=self.NClusters)
             gmm_model.fit(self.T)
-            preds = gmm_model.predict(self.Y[1])
-        elif self.classifier == 'birch':
+            self.fitCluster = gmm_model.fit(self.T)
+            self.preds = gmm_model.predict(self.Y[1])
+            self.cluster = self.preds
+        elif self.method == 'birch':
             birch_model = Birch(branching_factor=50, n_clusters= self.NClusters)
             birch_model.fit(self.T)
-            preds = birch_model.predict(self.Y[1])
+            self.fitCluster = birch_model.fit(self.T) 
+            self.preds = birch_model.predict(self.Y[1])
+            self.cluster = self.preds
 
         # for each of the clusters, find the labels of the data samples in the clusters
         # then look at the labels from the initially labeled data that are in the same
         # cluster. assign the cluster the label of the most frequent class.
         for i in range(self.NClusters):
-                xhat = self.X[i][preds]
+                xhat = self.X[i][self.preds]
                 mode_val,_ = stats.mode(xhat)
                 self.class_cluster[i] = mode_val
 
-    def _create_mclusters(self):
+    def _create_mclusters(self, inCluster, fitCluster):
         """
         Clustering options:
-        1. k-means
+        1. k-meansthanj
         2. GMM 
         3. Balanced Iterative Reducing and Clustering using Hierarchies (BIRCH)
         
@@ -143,17 +154,52 @@ class MClassification():
         y = label for a set of data points
         """
         
-    def _classify(self):
-        pass
-
-    def _euclidean_distance(self):
-        pass
-
-
-    def run(self, Xt, Yt, Ut):
+    def _classify(self, trainData, trainLabel, testData):
         """
+        Inputs include training data, training label, test data
+        Two classifiers 
+        1. K Nearest Neighbor
+        2. Support Vector Machine
         """
-        self.classifier
+        if len(trainData) >= len(trainLabel):
+            indx = np.unique(np.argwhere(trainLabel)[:,0])
+            trainData = trainData[indx]
+        elif len(trainLabel) >= len(trainData):
+            indx = np.unique(np.argwhere(trainData)[:,0])
+            trainLabel = trainLabel[indx]
+        
+        if self.classifier == 'knn':
+            knn = KNeighborsClassifier(n_neighbors=10).fit(trainData, trainLabel)   # KNN.fit(train_data, train label)
+            predicted_label = knn.predict(testData)
+        elif self.classifier == 'svm':
+            svm_mdl = SVC(gamma='auto').fit(trainData, trainLabel)                  # fit(Xtrain, X_label_train)
+            predicted_label = svm_mdl.predict(testData)
+
+        return predicted_label
+
+    def _euclidean_distance(self, inCluster, fitCluster, x, y):
+        """
+        inCluster = cluster model 
+        fitCluster = fitted model
+        x = datastream
+        y = label
+        """
+        
+        if self.method == 'kmeans':
+            cluster_centroids = {}
+            cluster_radii = {} 
+            for cluster in list(y):
+                cluster_centroids[cluster] = list(zip(inCluster.cluster_centers_[:,0], inCluster.cluster_centers_[:,1]))[cluster]
+                cluster_radii[cluster] = max([np.linalg.norm(np.subtract(i, cluster_centroids[cluster])) for i in zip(x[fitCluster == cluster, 0], x[fitCluster == cluster, 1])])
+        elif self.method == 'gmm':
+            pass # need to determine how to calc radii for gmm 
+        elif self.method == 'birch':
+            pass # need to determine how calc radii for birch
+
+    def run(self):
+        """
+        Micro-Cluster Classification
+        """
         """
         1. Get first set of labeled data T 
         2. Create MCs of the labeled data of first data T
@@ -163,10 +209,15 @@ class MClassification():
                 1.) if the added x_t to the MC does not exceed the MC radius -> update the (N, LS, SS )
                 2.) if the added x_t to the MC exceeds radius -> new MC carrying yhat_t is created to carry x_t
             c. The 
-
         """
 
+        past_centroid = self.cluster
+        yhat = self._classify(trainData=self.T, trainLabel=self.preds, testData=self.X[1])
+        
+        # temp_current_cluster = KMeans(n_clusters=self.NClusters, init=past_centroid, n_init=1).fit(self.X[0]).cluster_centers_
+        
 
-run_mclass = MClassification(classifier='gmm', dataset='UG_2C_2D')
+# test mclass
+run_mclass = MClassification(classifier='svm', method = 'kmeans', dataset='UG_2C_2D').run()
 
 # %%
