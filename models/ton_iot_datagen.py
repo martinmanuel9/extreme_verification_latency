ac6a84982b85771024009203e5b84558eb007817
Martin Lopez <78863793+martinmanuel9@users.noreply.github.com>
2023-07-21 09:37:23 -0700

    update to run in docker in server

--- models/ton_iot_datagen.py
+++ models/ton_iot_datagen.py
@@ -48,7 +48,7 @@ class TON_IoT_Datagen():
 
     def change_directory(self):
         path = str(Path.home())
-        path =  '../data/TON_IoT_Data/'
+        path =  '/data/TON_IoT_Data/'
         os.chdir(path)
 
     def import_data(self):
